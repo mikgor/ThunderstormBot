12f9b9358e2e24f376c94ed3148a709a62d0919a
mikgor <mikgorzny@gmail.com>
2019-07-03 15:44:15 +0200

    Add UserStormEvent model, notification constraints

--- TSBserv/models.py
+++ TSBserv/models.py
@@ -1,12 +1,45 @@
 from django.db import models
+from django.utils import timezone
+from django.db.models.signals import post_init
+
+class UserStormEvent(models.Model):
+    active = models.BooleanField(default=False)
+    updatedAt = models.DateTimeField(default=timezone.now, null=True)
+    distance = models.FloatField(default=0)
+    latitudeDirection = models.CharField(max_length=1)
+    lontitudeDirection = models.CharField(max_length=1)
+
+    def UpdateDetails(self, details):
+        self.distance = details['distance']
+        self.latitudeDirection = details['latDirection']
+        self.lonDirection = details['lonDirection']
+        self.lastStormEvent = timezone.now()
+        self.save()
+
+    def SetStatus(self):
+        self.active = not self.active
+        self.save()
+
+    def IsUpdated(self, details):
+        if self.distance == details['distance'] or self.latitudeDirection == details['latDirection'] or self.lonDirection == details['lonDirection']:
+            return False
+        return True
 
 class User(models.Model):
     messengerId = models.IntegerField()
     latitude = models.FloatField()
     longitude = models.FloatField()
     active = models.BooleanField(default=True)
+    dateJoined = models.DateTimeField(default=timezone.now, null=True)
+    stormEvent = models.OneToOneField(UserStormEvent, on_delete=models.CASCADE)
 
     def UpdateLocation(self, latitude, longitude):
         self.latitude = latitude
         self.longitude = longitude
         self.save()
+
+def UserAddUserStormEventObject(**kwargs):
+   instance = kwargs.get('instance')
+   instance.stormEvent = UserStormEvent.objects.create()
+
+post_init.connect(UserAddUserStormEventObject, User)
--- TSBserv/views.py
+++ TSBserv/views.py
@@ -6,6 +6,8 @@ from .classes.weatherApiHandler import WeatherApiHandler
 from .classes.messengerApiHandler import MessengerApiHandler
 from .classes.city import City
 import requests
+from django.utils import timezone
+import datetime
 
 def SendNotifications(request):
     ip = ''
@@ -17,20 +19,34 @@ def SendNotifications(request):
     if ip not in ALLOWED_API_REQUEST_IP:
         return HttpResponse("Access denied")
     for user in User.objects.all():
+        print(user.stormEvent.active)
+        print(user.stormEvent.updatedAt)
+        print(user.stormEvent.distance)
+        print(user.stormEvent.latitudeDirection)
+        print(user.stormEvent.lontitudeDirection)
+        if user.stormEvent.active and (timezone.now()-user.stormEvent.updatedAt).seconds < 300:
+            continue
         apiRequest = WeatherApiHandler().Request("lat="+str(user.latitude)+"&lon="+str(user.longitude))
         mainCity = City(apiRequest["list"][0])
         distance = 50
         stormFound = False
+        stormDetails = ""
         for city in apiRequest["list"]:
             currentCity = City(city)
             if currentCity.Storm:
-                currentDist = currentCity.Calculate(mainCity.Lat, mainCity.Lon)['distance']
-                if currentDist < distance and currentDist > 0:
-                    distance = currentDist
+                stormDetails = currentCity.Calculate(mainCity.Lat, mainCity.Lon)
+                if stormDetails['distance'] < distance and stormDetails['distance'] > 0:
+                    distance = stormDetails['distance']
                     stormFound = True
         if stormFound:
-            print(distance)
-        MessengerApiHandler().SendResponseMessage(user.messengerId, mainCity.Name)
+            if not user.stormEvent.active:
+                user.stormEvent.SetStatus()
+            if user.stormEvent.IsUpdated(stormDetails):
+                MessengerApiHandler().SendResponseMessage(user.messengerId, "Wykryto burze " + str(round(distance, 1)) +" kilometrów od " + mainCity.Name + ". Kierunek: " + stormDetails['latDirection']+ stormDetails['lonDirection'])
+            user.stormEvent.UpdateDetails(stormDetails)
+        else:
+            if user.stormEvent.active:
+                user.stormEvent.SetStatus()
     return HttpResponse("OK")
 
 def IncomingMessage(request):
@@ -47,14 +63,14 @@ def IncomingMessage(request):
     if messageType is None or messengerId is None:
         return HttpResponse("Empty field(s)")
     if messageType == 1:
-        r = MessengerApiHandler().SendResponseMessage(messengerId, "Welcome. You have been registered. Send your location to get started.")
+        r = MessengerApiHandler().SendResponseMessage(messengerId, "Welcome. Send your location to sign up.")
     if messageType == 2:
         latitude = request.GET.get('latitude', None)
         longitude = request.GET.get('longitude', None)
         if latitude is None or latitude is None:
             return HttpResponse("Empty field(s)")
         if not User.objects.filter(messengerId=messengerId).exists():
-            User.objects.create(messengerId=messengerId, latitude=latitude, longitude=longitude)
+            user = User.objects.create(messengerId=messengerId, latitude=latitude, longitude=longitude)
         else:
             User.objects.get(messengerId=messengerId).UpdateLocation(latitude, longitude)
         r = MessengerApiHandler().SendResponseMessage(messengerId, "Location received. Thank you :).")
